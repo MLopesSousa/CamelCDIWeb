137f06cc93403299b143c5c74434c891ce0b2603
Marcelo Lopes <marcelo.lopes@sefaz.ce.gov.br>
2018-02-05 17:40:40 -0300

    second commit

---
+++ src/marcelo/lopes/de/sousa/lima/BancoMarcelo.java
@@ -0,0 +1,12 @@
+package marcelo.lopes.de.sousa.lima;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class BancoMarcelo {
+	public String doSomething() {
+		//
+		return "Fui no Banco !!!";
+		
+	}
+}
--- src/marcelo/lopes/de/sousa/lima/Contexto.java
+++ src/marcelo/lopes/de/sousa/lima/Contexto.java
@@ -7,6 +7,8 @@ import javax.ejb.Startup;
 import javax.inject.Inject;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
 @Startup
@@ -16,24 +18,46 @@ public class Contexto {
 	@Inject
 	private CamelContext camelContext;
 
+	@Inject
+	private BancoMarcelo banco;
+
 	@PostConstruct
 	private void start() {
 
 		try {
-
+			
+			/**
+				A cada 5 segundos chame o bean: 'Bean 01' e o resultado jogue na fila: 'direct:entrada'
+			**/	
 			camelContext.addRoutes(new RouteBuilder() {
 				@Override
 				public void configure() throws Exception {
-					from("quartz2://timer1?cron=0/2+*+*+*+*+?").setBody().simple("I was fired at ${header.fireTime}")
-							.to("direct:entrada");
+					from("quartz2://timer1?cron=0/5+*+*+*+*+?")
+					.routeId("Rota produtora")
+					.bean(banco, "doSomething")
+					.id("Bean 01")
+					.to("direct:entrada").id("direct:entrada");
 				}
 
 			});
 
+			
+			/** 
+			 	Consuma da fila: 'direct:entrada', passe a mensagem para um processor: 'Processo 01' e jogue no sysout!
+			**/
 			camelContext.addRoutes(new RouteBuilder() {
 				@Override
 				public void configure() throws Exception {
-					from("direct:entrada").routeId("Rota consumidora").to("stream:out");
+					from("direct:entrada").routeId("Rota consumidora")
+					.process(new Processor() {
+						@Override
+						public void process(Exchange ex) throws Exception {
+							ex.getIn()
+							.setBody(ex.getIn().getBody().toString().concat(" + Alterado no processor !!!!"));
+						}
+					})
+					.id("Processo 01")
+					.to("stream:out").id("stream:out");
 				}
 			});
 
@@ -42,19 +66,26 @@ public class Contexto {
 			e.printStackTrace();
 		}
 
-		System.out.println("Iniciando contexto !!!");
-
+		
+		
+		
+		/** 
+		  	Iniciando o contexto
+		**/
 		try {
+			System.out.println("Iniciando contexto !!!");
 			camelContext.start();
 		} catch (Exception e) {
 			System.out.println("Erro ao iniciar contexto !!!");
 			e.printStackTrace();
 		}
 
-		// ProducerTemplate pc = camelContext.createProducerTemplate();
-		// pc.sendBody("direct:entrada", "Conteudo da mensagem !!!");
 	}
 
+
+	/**
+	 		Destruindo o contexto
+	**/
 	@PreDestroy
 	private void stop() {
 		System.out.println("Destruindo contexto !!!");
